8ee22468f470bb09d40151f226484dbd0de0aed3
emptyhhc <emptyhhc@163.com>
2018-08-20 11:47:51 +0800

    new commit

--- LogisticRegression.py
+++ LogisticRegression.py
@@ -89,7 +89,9 @@ def main():
     feat_num = 4000
     weights = np.random.rand(feat_num+1)
     #in_data = sys.argv[1]
-    in_data = '0806'
+
+    in_data = '0821'
+
     weights = train(in_data, weights, batchSize, feat_num)
     printModel('model.txt', weights)
 
